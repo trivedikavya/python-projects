6606c3a489d33c8389b3ecee2baff22d1998d564
KAVYA TRIVEDI ⚡ <149308388+trivedikavya@users.noreply.github.com>
2024-05-21 18:40:10 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,5 @@
-#created by kavya trivedi
 import random
-
+#CREATED BY KAVYA TRIVEDI
 def greet():
     responses = ["Hi there!", "Hello!", "Hey! How can I help you today?"]
     return random.choice(responses)
@@ -12,12 +11,15 @@ def farewell():
 def provide_info(topic):
     info = {
         "weather": "The weather is currently sunny and 25°C.",
-        "time": "your system has watch check it",
-        "news": "Here are the latest headlines..."
-        # Add more topics and corresponding information as needed #created by kavya trivedi
+        "time": "Your system has watch check it.",
+        "news": "Here are the latest headlines... ",
+        "age": "I'm just a bot, so I don't have an age, but if you're asking for my version, it's 2.",
+        "name": "I'm ChatBot K-1.1.",
+        "owner": "My owner is KAVYA TRIVEDI.",
+        "contact": "You can contact Kavya Trivedi on Instagram: @kavyatrivedi.og or on github=trivedikavya"
     }
     return info.get(topic, "I'm sorry, I don't have information on that topic.")
-#created by kavya trivedi
+#CREATED BY KAVYA TRIVEDI
 def chat():
     print("Bot: " + greet())
     while True:
@@ -32,9 +34,18 @@ def chat():
         elif "time" in user_input:
             print("Bot: " + provide_info("time"))
         elif "news" in user_input:
-            print("Bot: " + provide_info("news")) #created by kavya trivedi
+            print("Bot: " + provide_info("news"))
+        elif "age" in user_input:
+            print("Bot: " + provide_info("age"))
+        elif "name" in user_input:
+            print("Bot: " + provide_info("name"))
+        elif "owner" in user_input:
+            print("Bot: " + provide_info("owner"))
+        elif "contact" in user_input:
+            print("Bot: " + provide_info("contact"))
         else:
-            print("Bot: I'm just a simple chatbot. I don't understand much. Type 'exit' to end the conversation.") #created by kavya trivedi
+            print("Bot: I'm just a simple chatbot. which is created by KAVYA TRIVEDI. You can ask me about me, my owner and some basic questions like weather, time, news. I don't understand too much. so you can Type 'exit' to end the conversation.")
+            #CREATED BY KAVYA TRIVEDI
 
-if _name_ == "_main_":
-    chat() #created by kavya trivedi
+if __name__ == "__main__":
+    chat()
