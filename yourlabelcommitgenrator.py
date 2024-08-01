8c72c5a3173f472b54eb81bc1b49be428cb0ed47
KAVYA TRIVEDI ⚡ <149308388+trivedikavya@users.noreply.github.com>
2024-08-01 21:40:18 +0530

    Update yourlabelcommitgenrator.py

--- yourlabelcommitgenrator.py
+++ yourlabelcommitgenrator.py
@@ -1,3 +1,4 @@
 def get_user_input():
     language = input("Enter the programming language: ")
-  
+    commit_message = input("Enter the commit message: ")
+    print("Enter the code (end input with a single 'EOF' line):")
