0e8b868c837be035102f9542481acd23cb9e84d3
KAVYA TRIVEDI ⚡ <149308388+trivedikavya@users.noreply.github.com>
2024-11-02 13:05:21 +0530

    Update asciiconverter.py

--- asciiconverter.py
+++ asciiconverter.py
@@ -1,3 +1,4 @@
 #  ascii value ord() mate a use thay  first project 
 user = input("enter the word want to convert ")
 print("The ASSCI value is " , ord(user))
+# small and sweet code which do next lv things 
